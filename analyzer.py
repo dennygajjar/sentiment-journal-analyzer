145433e647d11bc98f21a84809e2a855b528b9e2
Denny Gajjar <dennygajjar1@gmail.com>
2025-10-05 06:37:53 +0530

    Create analyzer.py

---
+++ analyzer.py
@@ -0,0 +1,6 @@
+text_to_analyze = TextBlob("I am feeling happy and excited to start my first project!")
+
+sentiment = text_to_analyze.sentiment
+
+print("Text: ", text_to_analyze)
+print("Sentiment Polarity: ", sentiment.polarity)
